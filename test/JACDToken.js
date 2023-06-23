512f7cc4cbabce4ed36913eed8a2582d13e046f6
D-Brack <derrek.brack@protonmail.com>
2023-06-23 05:55:56 -0500

    Add & Test Minting Functionality in Token

--- test/JACDToken.js
+++ test/JACDToken.js
@@ -1,17 +1,21 @@
 const { expect } = require('chai')
 const { ethers } = require('hardhat')
+const { CompilationJobCreationErrorReason } = require('hardhat/types')
 
 const tokens = (amount) => {
   return ethers.utils.parseUnits(amount.toString(), 'ether')
 }
 
 describe('JACD Token', () => {
-  const NAME = 'Jadu Ava Charities Dao'
+  const NAME = 'Jadu Avas Charities DAO'
   const SYMBOL = 'JACD'
 
-  let jacd, transaction, result
+  let jacd, owner, user, transaction, result
 
   beforeEach(async () => {
+    const accounts = await ethers.getSigners()
+    owner = accounts[0]
+    user = accounts[1]
 
     const JACD = await ethers.getContractFactory('JACDToken')
     jacd = await JACD.deploy(NAME, SYMBOL)
@@ -21,6 +25,41 @@ describe('JACD Token', () => {
     it('sets the name and symbol', async () => {
       expect(await jacd.name()).to.equal(NAME)
       expect(await jacd.symbol()).to.equal(SYMBOL)
+    })
+
+    it('returns 0 decimals for tokens', async () => {
+      expect(await jacd.decimals()).to.equal(0)
+    })
+
+    it('sets token total supply to 0', async () => {
+      expect(await jacd.totalSupply()).to.equal(0)
+    })
+  })
+
+  describe('Minting', () => {
+    describe('Success', () => {
+      beforeEach(async () => {
+        transaction = await jacd.connect(owner).mint(user.address, 1)
+        await transaction.wait()
+      })
+
+      it('increases total supply', async () => {
+        expect(await jacd.totalSupply()).to.equal(1)
+      })
+
+      it('increases user balance', async () => {
+        expect(await jacd.balanceOf(user.address)).to.equal(1)
+      })
+    })
+
+    describe('Failure', () => {
+      it('rejects invalid receiver address', async () => {
+        await expect(jacd.connect(owner).mint('0x0000000000000000000000000000000000000000', 1)).to.be.reverted
+      })
+
+      it('rejects calls from non-owner', async () => {
+        await expect(jacd.connect(user).mint(owner.address, 1)).to.be.reverted
+      })
     })
   })
 })
